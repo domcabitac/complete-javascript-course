96f9c09d5fc9ca0347bd74edac64f2c46806a9ed
Dominic Cabitac <dominic.cabitac@uoit.net>
2020-05-27 17:53:56 -0400

    1/4 finished section 5

---
+++ 5-advanced-JS/starter/script.js
@@ -0,0 +1,25 @@
+// Function constructor
+
+var Person = function(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+Person.prototype.calculateAge = function() {
+  console.log(2020 - this.yearOfBirth);
+};
+
+Person.prototype.lastName = 'Smith';
+
+var john = new Person('John', 1990, 'teacher');
+var jane = new Person('Jane', 1968, 'designer');
+var mark = new Person('Mark', 1951, 'retired');
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
